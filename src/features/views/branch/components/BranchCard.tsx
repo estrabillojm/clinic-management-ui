266d7f808f1bb4d5b7b9b58c79bebc9b5328a672
John Michael Estrabillo <estrabillo.johnmichael@gmail.com>
2024-08-25 23:22:02 +0800

    switch name and description branch

--- src/features/views/branch/components/BranchCard.tsx
+++ src/features/views/branch/components/BranchCard.tsx
@@ -15,8 +15,8 @@ const BranchCard = ({ id, branchName, description, clinicId } : Props) => {
                 <div className="border-4 border-secondaryBg rounded-lg pb-4 px-2 pt-8 hover:bg-slate-300 transition-all relative overflow-hidden">
                     <FaClinicMedical className="absolute h-32 w-40 bottom-[-20px] right-1 text-secondaryBg z-0"/>
                     <div className="border-l-2 border-gray-300 pl-2 z-10 relative">
-                        <h2 className="text-xl font-bold text-primary">{ branchName }</h2>
-                        <h3 className="text-regular italic">{ description }</h3>
+                        <h2 className="text-xl font-bold text-primary">{ description }</h2>
+                        <h3 className="text-regular italic">{ branchName }</h3>
                     </div>
                 </div>
             </Link>
