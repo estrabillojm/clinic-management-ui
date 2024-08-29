05be6fc22747ffcdbc65342ed80d513abda9c57e
sorianokarlb <kbsoriano98@gmail.com>
2024-08-29 21:39:07 +0800

    partial report

--- src/features/views/patient/components/MedicalCertificate.tsx
+++ src/features/views/patient/components/MedicalCertificate.tsx
@@ -1,4 +1,5 @@
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { Page, Text, View, Document, StyleSheet, Image } from '@react-pdf/renderer';
+import clinicLogo from '../../../../assets/docpie_general.jpg';
 
 const styles = StyleSheet.create({
   page: {
@@ -6,9 +7,37 @@ const styles = StyleSheet.create({
   },
   section: {
     margin: 5,
-    padding: 5,
     flexGrow: 1, 
     fontSize: 12
+  },
+  headerSection: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 20
+  },
+  headerTextAlignment: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  headerTopText: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+  title: {
+    textAlign: 'center',
+    borderTop: '2px solid black',
+    borderBottom: '2px solid black',
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
   }
 });
 
@@ -16,7 +45,30 @@ const MedicalCertificate = (history: any, patientDetails: any) => (
   <Document>
     <Page size="A4" style={styles.page}>
       <View style={styles.section}>
-        <Text>MED CERT</Text>
+        <View style={styles.headerSection}>
+          <Image 
+          style={styles.image}
+          src={clinicLogo}
+          />
+          <View style={styles.headerTextAlignment}>
+            <Text style={styles.headerTopText}>
+              Sylvia Pamela Juan-Samiley, OD, MD, DipHLM, DPCOM, CHA
+            </Text>
+            <Text>
+              Optometrist/Occupational Medicine/Family Physician
+            </Text>
+            <Text>
+              Stall 3, 093 Henson Avenue, Fatima Village, Porac Pampanga
+            </Text>
+          </View>
+        </View>
+        <View style={styles.title}>
+          <Text>MEDICAL CERTIFICATE</Text>
+        </View>
+        <View>
+          <Text>To Whom It May Concern,</Text>
+        </View>
+        
         <Text>
           {/* REMOVE THIS JSON.stringify(history) AND JSON.stringify(patientDetails) */}
           { JSON.stringify(history) }
