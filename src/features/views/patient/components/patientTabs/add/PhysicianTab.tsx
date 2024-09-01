f6d8ec1dc146a8ead65fc67ca3a03317a2782e02
John Michael Estrabillo <estrabillo.johnmichael@gmail.com>
2024-09-01 11:45:08 +0800

    add recommendation field

--- src/features/views/patient/components/patientTabs/add/PhysicianTab.tsx
+++ src/features/views/patient/components/patientTabs/add/PhysicianTab.tsx
@@ -16,20 +16,17 @@ type PhysicianList = {
 };
 
 const PhysicianTab = ({ selectedTab, clinicId }: any) => {
-  const {
-    data: physicians,
-    isLoading,
-  } = useGetPhysicianListQuery(clinicId);
+  const { data: physicians, isLoading } = useGetPhysicianListQuery(clinicId);
   const [physicianList, setPhysicianList] = useState<PhysicianList[]>([]);
 
   const data = {
     physicianId: "",
-    remarks: ""
-  }
+    remarks: "",
+    recommendation: ""
+  };
 
   useEffect(() => {
     if (physicians) {
-
       setPhysicianList(
         physicians.result.users.map((physician: Physician) => ({
           label: `${physician.lastName.toUpperCase()}, ${physician.firstName.toUpperCase()}`,
@@ -44,26 +41,45 @@ const PhysicianTab = ({ selectedTab, clinicId }: any) => {
       {isLoading ? (
         <h3>Loading Data</h3> // Create a loader for the content
       ) : (
-        <div className={`grid grid-cols-12 gap-4 mb-8 ${selectedTab === 6 ? "block" : "hidden"}`}>
-          <div className="col-span-4">
-            <AutoComplete
-              label="Physician"
-              fieldName="physicianId"
-              isRequired={false}
-              options={physicianList}
-              defaultValue={data.physicianId}
-            />
+        <>
+          <div
+            className={`grid grid-cols-12 gap-4 mb-8 ${selectedTab === 6 ? "block" : "hidden"}`}
+          >
+            <div className="col-span-6">
+              <AutoComplete
+                label="Physician"
+                fieldName="physicianId"
+                isRequired={false}
+                options={physicianList}
+                defaultValue={data.physicianId}
+              />
+            </div>
           </div>
-          <div className="col-span-6">
-            <Input
-              type="string"
-              label="Remarks"
-              fieldName="remarks"
-              defaultValue={data.remarks}
-              isMultiline={true}
-            />
+
+          <div
+            className={`grid grid-cols-12 gap-4 mb-8 ${selectedTab === 6 ? "block" : "hidden"}`}
+          >
+
+            <div className="col-span-6">
+              <Input
+                type="string"
+                label="Remarks"
+                fieldName="remarks"
+                defaultValue={data.remarks}
+                isMultiline={true}
+              />
+            </div>
+            <div className="col-span-6">
+              <Input
+                type="string"
+                label="Recommendation"
+                fieldName="recommendation"
+                defaultValue={data.recommendation}
+                isMultiline={true}
+              />
+            </div>
           </div>
-        </div>
+        </>
       )}
     </>
   );
